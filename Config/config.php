372e59aa5a734fa852fe151c8cb23e8dfd76f947
alunauf <55720797+alunaufginf@users.noreply.github.com>
2020-09-28 19:35:14 -0500

    Create config.php
    el config

---
+++ Config/config.php
@@ -0,0 +1,8 @@
+<?php
+// DEFINO MIS CONSTANTES PARA LA CONEXION
+const BD_HOST = "localhost";
+const BD_USER = "root";
+const BD_PASS = "";
+const BD_NAME = "bdtiendavirtualg2";
+const BD_CHARSET = "charset=utf8";
+?>
